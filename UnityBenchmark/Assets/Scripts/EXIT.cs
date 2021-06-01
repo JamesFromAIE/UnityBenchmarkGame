1ef9356a812e67a6bbda82087d3bc7d4e5c8cced
James Webster <s200676@students.aie.edu.au>
2021-06-01 15:27:30 +1000

    Feature (greybox) : #2
    Created initial greybox environment.

---
+++ UnityBenchmark/Assets/Scripts/EXIT.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EXIT : MonoBehaviour
+{
+
+    // CLOSE GAME WHEN PLAYER CLICKS OBJECT
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Establishes a Raycast from the camera
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        // Something happens when hit
+        RaycastHit hit;
+        //print("hello 1");
+        if (Physics.Raycast(ray, out hit) && Input.GetMouseButtonDown(0))
+        {
+            if (hit.transform.position == transform.position)
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
